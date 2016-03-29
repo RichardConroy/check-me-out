3520543c0ca5387340e01d6f748eae5ec2292dab
Richard Conroy <richard.conroy@gmail.com>
2016-03-30 00:47:52 +0100

    Adding integration specs. Specs based on unimplemented lavender hearts rule are red

---
+++ spec/integration_spec.rb
@@ -0,0 +1,39 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+require 'checkout'
+require 'rule/total_greater_than'
+
+describe 'Integration Specs', type: :integration do
+	let(:item1) { double(:item1, price: 9.25, code: '001', name: 'Lavender Heart')}
+	let(:item2) { double(:item2, price: 45, code: '002', name: 'Personalised Cufflinks')}
+	let(:item3) { double(:item3, price: 19.95, code: '003', name: 'Kids T-shirt')}
+
+	let(:gtr60_rule) { Rule::TotalGreaterThan.new 60 }
+	let(:promotion_rules) { [gtr60_rule] }
+
+	let(:checkout) { Checkout.new promotion_rules}
+
+	it 'computes total correctly when one of each product is in the checkout' do
+		checkout.scan item1
+		checkout.scan item2
+		checkout.scan item3
+		expect(checkout.total).to eq 66.78
+	end
+
+	it 'computes total correctly when there are 2 lavender hearts in the checkout' do
+		checkout.scan item1
+		checkout.scan item3
+		checkout.scan item1
+		expect(checkout.total).to eq 36.95
+	end
+
+	it 'computes total correctly when both rules are in effect' do
+		checkout.scan item1
+		checkout.scan item2
+		checkout.scan item1
+		checkout.scan item3
+		expect(checkout.total).to eq 73.76
+	end
+
+
+end
